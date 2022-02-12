a9bcd8e2e8a8046f0d7fd46dd624535bddc1a6d4
Attila Toth <tthttl@gmail.com>
2022-02-12 13:04:26 +0100

    add appinsights

--- nwa-api-booking/nwa-api-booking-post/index.ts
+++ nwa-api-booking/nwa-api-booking-post/index.ts
@@ -1,7 +1,12 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import appInsights = require("applicationinsights");
+
+appInsights.setup(process.env.APPLICATIONINSIGHTS_CONNECTION_STRING).start();
+const client = appInsights.defaultClient;
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log(req.body);
+    client.trackEvent({name: "NWA-API-BOOKING-POST Request Received", properties: {requestBody: req.body}});
     //save body as booking item in cosmos db
     //if success => 201 else 500
     context.res = {
